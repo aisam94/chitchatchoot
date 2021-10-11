ca237e7471ce3e8448b003c684626bd420bed39c
Pulldtrigger <pulldtrigger94@gmail.com>
2021-10-11 12:25:04 +0800

    create 1 to 1 chat array in db

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,18 +1,50 @@
 import { DotsVerticalIcon, PlusIcon } from "@heroicons/react/outline";
 import { UserCircleIcon, ChatAltIcon } from "@heroicons/react/solid";
-import { auth } from "../firebase";
+import { auth, db } from "../firebase";
+import {
+  doc,
+  addDoc,
+  setDoc,
+  collection,
+  query,
+  where,
+} from "@firebase/firestore";
 
 import * as EmailValidator from "email-validator";
+import { useAuthState } from "react-firebase-hooks/auth";
+// import { useCollection } from "react-firebase-hooks/firestore";
 
 const Sidebar = () => {
+  const [user] = useAuthState(auth);
+
+  //query for chat that has user email in chats db
+  const chatCollection = collection(db, "chats");
+  const queryChat = query(
+    chatCollection,
+    where("users", "array-contains", user.email)
+  );
+  // const [chatsSnapshot] = useCollection(queryChat);
+
+  //create chat by entering recipient email via prompt
   const createChat = () => {
     const input = prompt("Enter email address for user to chat with");
 
     if (!input) return null;
-    if (EmailValidator.validate(input)) {
+    if (EmailValidator.validate(input) && input !== user.email) {
       //add chats to db chats collection
+      // setDoc(doc(chatCollection), { users: [user.email, input] });
+      setDoc(doc(chatCollection, input), { users: [user.email, input] }); // this one set recipient email as doc name
+      // addDoc(chatCollection, { users: [user.email, input] });
     }
   };
+
+  // const chatAlreadyExist = (recipientEmail: any) => {
+  //   !!chatsSnapshot?.docs.find((chat: any) => {
+  //     chat.data().users.find((user: any) => user === recipientEmail)?.length >
+  //       0;
+  //   });
+  // };
+
   return (
     <div className="bg-red-300 w-1/4 h-full">
       {/*User logo circle*/}
